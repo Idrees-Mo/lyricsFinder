f2ca6f72fab575fc0da8a6d529ff1952b0652226
muhammad idrees <idris@y7mail.com>
2019-06-01 00:37:31 +0930

    Updates

---
+++ precache-manifest.b9013104b27e721556d0af8b6503ec35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07606199390004e3ede5cb61bcdb6c04",
+    "url": "/lyricsFinder/index.html"
+  },
+  {
+    "revision": "2f152fcbee183442fc84",
+    "url": "/lyricsFinder/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "dd3d7580f0fba4f541c1",
+    "url": "/lyricsFinder/static/js/2.cbbffc2f.chunk.js"
+  },
+  {
+    "revision": "2f152fcbee183442fc84",
+    "url": "/lyricsFinder/static/js/main.8d661b72.chunk.js"
+  },
+  {
+    "revision": "12f64fb58aa85384612d",
+    "url": "/lyricsFinder/static/js/runtime~main.24e83bd2.js"
+  }
+]);
